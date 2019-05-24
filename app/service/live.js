0c3b50525433f5f8df42f0ffff7bd95bd8966f9f
WozHuang <909695699@qq.com>
2019-05-24 16:10:31 +0800

    fix bug
    由于虎牙html中的js有问题导致错误，因此修改

--- app/service/live.js
+++ app/service/live.js
@@ -3,6 +3,8 @@
 const Service = require('egg').Service;
 
 class LiveService extends Service {
+
+  // 手动处理html字符，哪天报错了只能再改了，因为没有接口，数据都是从html中截取出来的
   async getLiveDetail(roomHost) {
     const ctx = this.ctx;
     const $ = await ctx.helper.cheerioLoad(`https://www.huya.com/${roomHost}`);
@@ -17,7 +19,8 @@ class LiveService extends Service {
       .replace(/window\.performanceInfo\._hmt.push[^;]*?;/, '')
       .replace(/window\.TT_LIVE_TIMING[^;]*?;/, '')
       .replace(/&quot;/g, '\\"')
-      .replace(/var /g, 'result.');
+      .replace(/var /g, 'result.')
+      .replace(/G_info\.camp/g, 'result.G_info.camp');
     // eslint-disable-next-line no-new-func
     return (Function(`const result = {};${script};return result`))();
   }
